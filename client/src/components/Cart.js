062e78c8214cdcf318c90b4cd9782585845e8e16
Karl <karliafrancesco@gmail.com>
2022-08-01 19:50:47 -0400

    cart not done

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,4 +1,28 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
 const Cart = () => {
+  const [cartItem, setCartItem] = useState(null);
+  const [status, setStatus] = useState("loading");
+  const cart = JSON.parse(localStorage.getItem(`cartID`));
+
+  useEffect(() => {
+    fetch(`/getCartItems/${cart}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCartItem(data);
+        console.log(data);
+        setStatus("idle");
+      })
+      .catch((err) => {
+        setStatus("error");
+      });
+  }, []);
+
+  if (status === "loading") {
+    return <div>loading</div>;
+  }
+
   return <div>cart</div>;
 };
 
